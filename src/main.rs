7d6cdd0a14f1795938233ff802bd28cc15f10386
Dima <dima.rets@ballys.com>
2025-04-18 17:43:24 +0100

    Clean exit, print reason of stop

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use nix::fcntl::OFlag;
 use std::os::unix::io::{AsRawFd, RawFd};
 use anyhow::{bail, Result};
 use copperline::Copperline;
-use nix::sys::wait::waitpid;
-use nix::sys::ptrace::{attach, cont, traceme};
+use nix::sys::wait::{waitpid, WaitStatus};
+use nix::sys::ptrace::{attach, cont, detach, traceme};
 use nix::unistd::{execv, fork, ForkResult, pipe2, Pid};
+use nix::sys::signal::{kill, Signal};
+
 
 fn main() -> Result<()> {
     let mut args = std::env::args();
@@ -21,7 +23,7 @@ fn main() -> Result<()> {
         let pid_str = args.nth(0).unwrap();
         let pid_num = pid_str.parse::<i32>().expect("PID should be a number");
         let pid = Pid::from_raw(pid_num);
-        let mut process = Process { pid, status: None };
+        let mut process = Process { pid, status: None, terminate_on_end: false };
         process.attach()?;
         main_loop(&mut process)?;
     } else {
@@ -38,6 +40,8 @@ fn main_loop(process: &mut Process) -> Result<()> {
         if !line.is_empty() {
             if line.starts_with("continue") {
                 process.resume()?;
+                let reason = process.wait_on_signal()?;
+                print_stop_reason(process, reason);
             }
         }
         cl.add_history(line);
@@ -46,6 +50,18 @@ fn main_loop(process: &mut Process) -> Result<()> {
     Ok(())
 }
 
+fn print_stop_reason(process: &mut Process, reason: StopReason) {
+    println!("Process {} ", process.pid.to_string());
+
+    match reason.reason {
+        Pstatus::Stopped => println!("stopped with signal {}", reason.signal),
+        Pstatus::Running => panic!("unreachable print_stop_reason"),
+        Pstatus::Terminated => println!("terminated with signal: {}", reason.signal),
+        Pstatus::Exited => println!("exited with status {}", reason.status),
+    }
+}
+
+#[derive(PartialEq, Eq)]
 enum Pstatus {
     Stopped,
     Running,
@@ -53,9 +69,34 @@ enum Pstatus {
     Exited
 }
 
+struct StopReason {
+    reason: Pstatus,
+    status: u8,
+    signal: String,
+}
+
 pub struct Process {
     pid: Pid,
     status: Option<Pstatus>,
+    terminate_on_end: bool,
+}
+
+impl Drop for Process {
+    fn drop(&mut self) {
+        if self.pid.as_raw() != 0 {
+            if self.terminate_on_end {
+                let _ = kill(self.pid, Signal::SIGKILL);
+                let _ = waitpid(self.pid, None);
+            } else {
+                if self.status == Some(Pstatus::Running) {
+                    let _ = kill(self.pid, Signal::SIGSTOP);
+                    let _ = waitpid(self.pid, None);
+                }
+                let _ = detach(self.pid, None);
+                let _ = kill(self.pid, Signal::SIGCONT);
+            }
+        }
+    }
 }
 
 impl Process {
@@ -66,20 +107,16 @@ impl Process {
         }
 
         attach(pid)?;
-        waitpid(pid, None)?;
-
-        self.status = Some(Pstatus::Stopped);
+        self.wait_on_signal()?;
 
         println!("Attached to {}", pid);
         Ok(())
     }
 
     pub fn resume(&mut self) -> Result<()> {
-        let pid = self.pid;
         cont(self.pid, None)?;
 
         self.status = Some(Pstatus::Running);
-        waitpid(pid, None)?;
 
         println!("Resumed process: {}", self.pid);
         Ok(())
@@ -103,7 +140,7 @@ impl Process {
 
                 waitpid(child, None)?;
                 println!("Launched child process for {}: {}", program_path, child);
-                Ok(Process { pid: child, status: Some(Pstatus::Running) })
+                Ok(Process { pid: child, status: Some(Pstatus::Running), terminate_on_end: true })
             }
             Ok(ForkResult::Child) => {
                 nix::unistd::close(read_fd.as_raw_fd())?;
@@ -120,6 +157,29 @@ impl Process {
                 unreachable!();
             }
             Err(_) => bail!("Fork failed"),
+        }
+    }
+
+    fn wait_on_signal(&mut self) -> Result<StopReason>{
+        match waitpid(self.pid, None) {
+            Ok(wait_status) => {
+                match wait_status {
+                    WaitStatus::Exited(_pid, status) => {
+                        self.status = Some(Pstatus::Exited);
+                        Ok(StopReason { reason: Pstatus::Exited, status: status as u8, signal: "".to_string() })
+                    },
+                    WaitStatus::Signaled(_pid, signal, _) => {
+                        self.status = Some(Pstatus::Terminated);
+                        Ok(StopReason { reason: Pstatus::Terminated, status: 0, signal: signal.to_string() })
+                    },
+                    WaitStatus::Stopped(_pid, signal) => {
+                        self.status = Some(Pstatus::Stopped);
+                        Ok(StopReason { reason: Pstatus::Stopped, status: 0, signal: signal.to_string() })
+                    },
+                    _ => bail!("Process is not stopped: {}", self.pid),
+                }
+            },
+            Err(e) => bail!("waitpid failed {}", e),
         }
     }
 }
